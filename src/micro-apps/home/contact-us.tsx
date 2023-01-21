fd937f0192c2bb0916351766aa1430bc4c65b40d
Sarah Wang <sarahwang@sarahs-air.lan>
2023-01-20 22:50:25 -0800

    updated contact us page

--- src/micro-apps/home/contact-us.tsx
+++ src/micro-apps/home/contact-us.tsx
@@ -11,39 +11,69 @@ export const ContactUs = (props: any) => {
         return (
             <LayoutPage microApp={homeApp}>
                 <div className="v-contact-us">
-                    <div className="v-form">
-                        <div className="home-header" >Contact US</div>
-                        <div className="home-header-line" >___</div>
-                        <div className="v-summary">
-                            Tell us more about your environmental sustainability objectives and challenges. We will get back to you soon.
+
+         
+                <Row >
+                    <Col sm={6}>
+                        <div className="v-form">
+                            <div className="home-header" >Contact us</div>
+                            <div className="v-summary">
+                                Tell us more about your environmental sustainability objectives and challenges. We will get back to you soon.
+                            </div>
+                            <Row className="v-form-row">
+                                <Col className="v-form-col">
+                                    <Form.Control required={true} type="text" placeholder="Your name" />
+                                </Col>
+                            </Row>
+                            <Row className="v-form-row">
+                                <Col className="v-form-col">
+                                    <Form.Control required={true} type="text" placeholder="Your email" />
+                                </Col>
+                            </Row>
+                            <Row className="v-form-row">
+                                <Col className="v-form-col">
+                                    <Form.Control type="text" placeholder="Phone number" />
+                                </Col>
+                            </Row>
+                            <Row className="v-form-row">
+                                <Col className="v-form-col">
+                                    <Form.Control as="textarea" rows={5} placeholder="Tell us more:" />
+                                </Col>
+                            </Row>
+
                         </div>
-                        <Row className="v-form-row">
-                            <Col className="v-form-col">
-                                <Form.Control required={true} type="text" placeholder="Name:" />
-                            </Col>
-                        </Row>
-                        <Row className="v-form-row">
-                            <Col className="v-form-col">
-                                <Form.Control required={true} type="text" placeholder="Email:" />
-                            </Col>
-                        </Row>
-                        <Row className="v-form-row">
-                            <Col className="v-form-col">
-                                <Form.Control type="text" placeholder="Phone:" />
-                            </Col>
-                        </Row>
-                        <Row className="v-form-row">
-                            <Col className="v-form-col">
-                                <Form.Control as="textarea" rows={3} placeholder="Tell us more:" />
-                            </Col>
-                        </Row>
-                        <Row className="v-form-row">
-                            <Col className="v-form-col button-container">
-                                <Button onClick={handleClick}>Submit</Button>
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
+                    </Col>
+                    <Col sm={6}>
+                        <div className="contact">
+                            <div className="contact-1">
+                                General inquiries
+                                <p>contact@viridium.ai</p>
+                            </div>
+                            <div className="contact-2">
+                                Press & Media
+                                <p> press@viridium.ai</p>
+                            </div>
+                            <div className="contact-3">
+                                Location
+                                <p>1234 Viridium Street <br />
+                                    Green Island <br />
+                                    CA 94115 <br />USA</p>
+                            </div>
+                        </div>
+
+                    </Col>
+                </Row>
+                <Row  >
+
+                    <Col sm={6} className="v-form-col button-container">
+                        <Button onClick={handleClick}>Submit</Button>
+                    </Col>
+
+
+                    <Col sm={6}> 
+                    </Col>
+                </Row>   
+            </div>
             </LayoutPage>
         )
     }
